62072fbfb0fb635e26ef358c23984acf32a95876
Ubuntu <ubuntu@ip-172-31-5-194.us-east-2.compute.internal>
2019-07-18 22:49:43 +0000

    Train function working.

--- python/check.py
+++ python/check.py
@@ -17,10 +17,10 @@ import torch
 def train(epochs, loaders, model, optimizer, criterion, device, save_path, verbose=True):
     """
     Name: train
-    
+
     Parameters: 8
     _____________
-    
+
     epochs: int, # of training iterations
     loaders: dictionary, consists of trainloader, testloader and validloader
     model: lmao the model
@@ -32,61 +32,57 @@ def train(epochs, loaders, model, optimizer, criterion, device, save_path, verbo
     """
     ### Moving model to hardware accelerator
     model.to(device)
-    
+
     valid_loss_min = np.Inf
-    
+
     ### Training and Validation Loop
     for e in range(epochs):
         train_loss = 0.0
         valid_loss = 0.0
-        
-        # Train loop
-        model.train() 
+
+	# Train loop
+        model.train()
         for batch_idx, (feature, label) in enumerate(loaders['train']):
             feature, label = feature.to(device), label.to(device)
-            
+
             optimizer.zero_grad()
-            
-            log_ps, aux_outputs = model(feature)
-            loss1 = criterion(log_ps, label)
-            loss2 = criterion(aux_outputs, label)
-            loss = loss1 + 0.4 * loss2
-            
+
+            log_ps, _ = model(feature)
+            loss = criterion(log_ps, label)
+
             loss.backward()
             optimizer.step()
-            
+
             train_loss += (1 / (batch_idx + 1)) * (loss.item() - train_loss)
-        
-        # Validation loop  
-        model.eval() 
+
+        # Validation loop
+        model.eval()
         for batch_idx, (feature, label) in enumerate(loaders['valid']):
             feature, label = feature.to(device), label.to(device)
-           
-            log_ps, aux_outputs = model(feature)
-            loss1 = criterion(log_ps, label)
-            loss2 = criterion(aux_outputs, label)
-            loss = loss1 + 0.4 * loss2
-           
+
+            log_ps = model(feature)
+            loss = criterion(log_ps, label)
+
             valid_loss += (1 / (batch_idx + 1)) * (loss.item() - valid_loss)
-        
-        # Printint out results
+
+        # Print out results
         if verbose:
             print('Epoch: {} \tTraining Loss: {} \tValidation Loss: {}'.format(e + 1, train_loss, valid_loss))
-            
         if valid_loss < valid_loss_min:
             torch.save(model.state_dict(), save_path)
             valid_loss_min = valid_loss
-        
+            print('Saving Model...')
+
     return model
-    
+
 ### Testing method
 def test(loaders, model, criterion, device, verbose=True):
     """
     Name: test
-    
+
     Parameters: 5
     _____________
-    
+
     loaders: dictionary, consists of trainloader, testloader and validloader
     model: lmao the model
     criterion: torch.nn.{loss}
@@ -95,26 +91,26 @@ def test(loaders, model, criterion, device, verbose=True):
     """
     ### Moving model to hardware accelerator
     model.to(device)
-     
+
     # Initializing test results
     test_loss = 0.0
     correct = 0
     total = 0
-    
+
     # Test loop
     model.eval()
     for batch_idx, (feature, label) in enumerate(loaders['test']):
         feature, label = feature.to(device), label.to(device)
-        
-        log_ps = model(feature)
+
+        log_ps, _ = model(feature)
         loss = criterion(log_ps, label)
-        
+
         test_loss += (1 / (batch_idx + 1)) * (loss.item() - test_loss)
         pred = log_ps.data.max(1, keepdim=True)[1]
         correct += np.sum(np.squeeze(pred.eq(feature.data.view_as(pred))).cpu().numpy())
-        total += feature.size[0]        
-        
+        total += feature.size[0]
+
     if verbose:
         print('Test Loss: {}\n'.format(test_loss))
         print('Test Accuracy: {}%, ({}/{})'.format(100 * correct / total, correct, total))
-        
+
--- python/data.py
+++ python/data.py
@@ -35,7 +35,7 @@ check_transforms = transforms.Compose([
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
         ])
-        
+
 ### Loading files as a dataset
 trainset = datasets.ImageFolder('../data/train/', transform=train_transforms)
 testset = datasets.ImageFolder('../data/test/', transform=check_transforms)
