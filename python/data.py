04541c514af884bc98de56065c335a9fe7ff60b9
Shubham Kumar <kumar.shubham5504@gmail.com>
2019-07-17 20:22:15 -0700

    Changed Dimensions Of Images
    Google Inception-V3 uses (299, 299) images instead of (224, 224). I've modified the size so it fixes the dimension problem and resolves the following error:
    
    RuntimeError: Calculated padded input size per channel: (3 x 3). Kernel size: (5 x 5). Kernel size can't be greater than actual input size
    
    Shubham Kumar

--- python/data.py
+++ python/data.py
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 
 # training set
 train_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((299, 299)),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomVerticalFlip(p=0.5),
         transforms.RandomRotation(10),
@@ -30,7 +30,7 @@ train_transforms = transforms.Compose([
 
 # test and valid sets
 check_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((299, 299)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
