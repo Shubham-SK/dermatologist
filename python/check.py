267c1ca1d3eddaf5c4d563a98cb7ff2896ca78fc
Ubuntu <ubuntu@ip-172-31-5-194.us-east-2.compute.internal>
2019-07-19 20:26:27 +0000

    Configured aux_outputs

--- python/check.py
+++ python/check.py
@@ -47,8 +47,10 @@ def train(epochs, loaders, model, optimizer, criterion, device, save_path, verbo
 
             optimizer.zero_grad()
 
-            log_ps, _ = model(feature)
-            loss = criterion(log_ps, label)
+            log_ps, aux_outputs = model(feature)
+            loss1 = criterion(log_ps, label)
+            loss2 = criterion(aux_outputs, label)
+            loss = loss1 + 0.4 * loss2
 
             loss.backward()
             optimizer.step()
